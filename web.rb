e3c59e961a3995411e5bcdbae670f28d763f72fb
Uday Sagar Nadimpally <udaysagarn@gmail.com>
2014-02-10 03:47:31 +0530

    edit

--- web.rb
+++ web.rb
@@ -6,6 +6,6 @@ get '/' do
   This is a work in progress. Whatch this space."
 end
 
-get '/index1' do
-   redirect_to '/index.htm'
+get '/file' do
+   File.read(File.join('public', 'index.html'))
 end
